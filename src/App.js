8a2e5c3863a12886b7bb313ac2a83d0816681cee
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-13 14:41:22 +0300

    Add files for React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <h1>Birthday Reminder</h1>
+        </React.Fragment>
+    );
+};
+
+export default App;
