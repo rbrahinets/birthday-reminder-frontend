fc7f0b55e08821660f238294f41b2544d2a20016
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-13 14:51:57 +0300

    Add fetching data from server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get(
+                    `https://api-birthday-reminder-5xlo.onrender.com`
+                );
+                setData(result.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <React.Fragment>
             <h1>Birthday Reminder</h1>
+            <div>{data ? data.title : 'Wait...'}</div>
         </React.Fragment>
     );
 };
