09f229060e58afd078041551d90007cbe39f19d8
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:48:47 +0300

    Add routing for sign in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import PageNotFound from './pages/PageNotFound';
 
@@ -7,6 +8,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
