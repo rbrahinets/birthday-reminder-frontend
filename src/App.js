1d07488216bd2df0a6a1953829d78e3efb32d67c
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:46:43 +0300

    Add routing for page not found

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/sign-up" element={<SignUp />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </Router>
     );
