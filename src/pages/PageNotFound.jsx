49b1657464576ee91f834a8cf31885ba4a2a3e00
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:46:00 +0300

    Add page for not found error

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const PageNotFound = () => {
+    return (
+        <>
+            <h1>Oops...</h1>
+            <h2>Page Not Found</h2>
+        </>
+    );
+};
+
+export default PageNotFound;
