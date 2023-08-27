74c6580931d0c03f6c1abd046fe90486d2e7bc2a
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:43:08 +0300

    Add styles to page for sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './styles.css';
 import userService from '../services/UserService';
 
 const SingUp = () => {
