54c7236eef945786060dd25d3cc8911e08be92d6
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:50:02 +0300

    Add routing for main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import PageNotFound from './pages/PageNotFound';
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Main />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="*" element={<PageNotFound />} />
