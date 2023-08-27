df77379066f91a22cf08d70f6deac843268cf881
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 20:39:28 +0300

    Add service for fetching user data from server

---
+++ src/services/UserService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'https://api-birthday-reminder-5xlo.onrender.com/api/v1/users',
+});
+
+class UserService {
+    async getAll() {
+        return await API.get('/');
+    }
+
+    async findById(id) {
+        return await API.get(`/${id}`);
+    }
+
+    async save(admin) {
+        return await API.post('/', admin);
+    }
+
+    async update(id, admin) {
+        return await API.put(`/${id}`, admin);
+    }
+
+    async delete(id) {
+        return await API.delete(`/${id}`);
+    }
+}
+
+const userService = new UserService();
+
+export default userService;
