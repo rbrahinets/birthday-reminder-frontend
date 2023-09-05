4c2bcd16a8a127bbd4eef705c8df38126c7b3385
Rostyslav <rostyslav.brahinets@gmail.com>
2023-09-05 20:49:51 +0300

    Add button for back to main page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { baseUrl } from '../constants';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 const PageNotFound = () => {
+    const navigate = useNavigate();
+
+    const handleMainClick = () => {
+        navigate(`${baseUrl}/`);
+    };
+
     return (
         <>
             <Header />
             <h1>Oops...</h1>
             <h2>Page Not Found</h2>
+            <Button text={'Main'} onClick={handleMainClick} />
         </>
     );
 };
