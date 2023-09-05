38f7aececbfdc713f91460683fe9aeb9e627dfbc
Rostyslav <rostyslav.brahinets@gmail.com>
2023-09-05 20:41:00 +0300

    Add Header component to page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Header from '../components/Header';
 
 const PageNotFound = () => {
     return (
         <>
+            <Header />
             <h1>Oops...</h1>
             <h2>Page Not Found</h2>
         </>
