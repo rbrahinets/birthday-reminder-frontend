90e66e9ed29d444f43da2099121088775b8750fe
Rostyslav <rostyslav.brahinets@gmail.com>
2023-09-06 13:12:04 +0300

    Refactor rendering of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,45 +6,53 @@ import { baseUrl } from '../constants';
 const Header = () => {
     const currentPage = window.location.href.split('/').reverse()[0];
 
-    const handleSingOutClick = () => {
-        localStorage.removeItem('token');
-    };
-
     const getToken = () => {
         return localStorage.getItem('token');
+    };
+
+    const renderHeaderForUnauthenticatedUser = () => {
+        return (
+            <>
+                {currentPage !== 'sign-in' && (
+                    <Link to={`${baseUrl}/sign-in`} className={'link'}>
+                        Sign In
+                    </Link>
+                )}
+                {currentPage !== 'sign-up' && (
+                    <Link to={`${baseUrl}/sign-up`} className={'link'}>
+                        Sign Up
+                    </Link>
+                )}
+            </>
+        );
+    };
+
+    const renderHeaderForAuthenticatedUser = () => {
+        return (
+            <>
+                <Link to={`${baseUrl}/`} className={'link'}>
+                    Main
+                </Link>
+                <Link
+                    to={`${baseUrl}/sign-in`}
+                    onClick={handleSingOutClick}
+                    className={'link'}
+                >
+                    Sign Out
+                </Link>
+            </>
+        );
+    };
+
+    const handleSingOutClick = () => {
+        localStorage.removeItem('token');
     };
 
     return (
         <header>
             <h1>Birthday Reminder</h1>
-            {!getToken() && (
-                <>
-                    {currentPage !== 'sign-in' && (
-                        <Link to={`${baseUrl}/sign-in`} className={'link'}>
-                            Sign In
-                        </Link>
-                    )}
-                    {currentPage !== 'sign-up' && (
-                        <Link to={`${baseUrl}/sign-up`} className={'link'}>
-                            Sign Up
-                        </Link>
-                    )}
-                </>
-            )}
-            {getToken() && (
-                <>
-                    <Link to={`${baseUrl}/`} className={'link'}>
-                        Main
-                    </Link>
-                    <Link
-                        to={`${baseUrl}/sign-in`}
-                        onClick={handleSingOutClick}
-                        className={'link'}
-                    >
-                        Sign Out
-                    </Link>
-                </>
-            )}
+            {!getToken() && renderHeaderForUnauthenticatedUser()}
+            {getToken() && renderHeaderForAuthenticatedUser()}
         </header>
     );
 };
