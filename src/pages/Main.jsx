c28d5f124aad5313b40485e8f23cf7d24e1acb77
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-10 20:44:28 +0200

    Add sign in modal form to main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button';
+import SignInModal from '../components/SignInModal';
 import Footer from '../components/Footer';
 
 const Main = () => {
+    const [showSignInModal, setShowSignInModal] = useState(false);
+
+    const handleShowSignInModal = () => {
+        setShowSignInModal(true);
+    };
+
+    const handleHideSignInModal = () => {
+        setShowSignInModal(false);
+    };
+
     return (
         <>
             <center className={'content'}>
-                <Header />
+                <Header/>
                 <main>
                     <h1>hello friend</h1>
+                    <Button
+                        text={'Sign In'}
+                        onClick={handleShowSignInModal}
+                    />
+                    <SignInModal
+                        show={showSignInModal}
+                        onHide={handleHideSignInModal}
+                    />
                 </main>
-                <Footer className={'footer'} />
+                <Footer className={'footer'}/>
             </center>
         </>
     );
