e5ea93c5087ee068e3f25d347d48df2a6d670753
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-10 20:47:47 +0200

    Deleted unnecessary links from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import {useNavigate, Link} from 'react-router-dom';
 import './styles.css';
-import { baseUrl } from '../constants';
+import {baseUrl} from '../constants';
 
 const Header = () => {
     const navigation = useNavigate();
-    const currentPage = window.location.href.split('/').reverse()[0];
 
     const getToken = () => {
         return localStorage.getItem('token');
-    };
-
-    const renderHeaderForUnauthenticatedUser = () => {
-        return (
-            <>
-                {currentPage !== 'sign-in' && (
-                    <Link to={`${baseUrl}/sign-in`} className={'link'}>
-                        Sign In
-                    </Link>
-                )}
-                {currentPage !== 'sign-up' && (
-                    <Link to={`${baseUrl}/sign-up`} className={'link'}>
-                        Sign Up
-                    </Link>
-                )}
-            </>
-        );
     };
 
     const renderHeaderForAuthenticatedUser = () => {
@@ -55,7 +37,6 @@ const Header = () => {
             <h1 className={'main-link'} onClick={handleMainClick}>
                 Birthday Reminder
             </h1>
-            {!getToken() && renderHeaderForUnauthenticatedUser()}
             {getToken() && renderHeaderForAuthenticatedUser()}
         </header>
     );
