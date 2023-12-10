0a9bb6cb976062c40652f3aaa81614e71f722f24
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-10 21:50:20 +0200

    Delete unnecessary pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Main from './pages/Main';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import PageNotFound from './pages/PageNotFound';
 
 const url = '/birthday-reminder-frontend';
@@ -11,10 +9,8 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path={`${url}/`} element={<Main />} />
-                <Route path={`${url}/sign-in`} element={<SignIn />} />
-                <Route path={`${url}/sign-up`} element={<SignUp />} />
-                <Route path="*" element={<PageNotFound />} />
+                <Route path={`${url}/`} element={<Main/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </Router>
     );
