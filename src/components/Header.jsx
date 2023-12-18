dbe66891404ec5db72bbb3a79065543505fe1481
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-18 19:22:20 +0200

    Add modal forms for sign in/up for unauthenticated user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import {useNavigate, Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import './styles.css';
 import {baseUrl} from '../constants';
+import SignInModal from './SignInModal';
+import SignUpModal from './SignUpModal';
 
 const Header = () => {
+    const [showSignInModal, setShowSignInModal] = useState(false);
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+
     const navigation = useNavigate();
 
     const getToken = () => {
         return localStorage.getItem('token');
     };
+
+    const handleShowSignInModal = () => {
+        setShowSignInModal(true);
+    };
+
+    const handleHideSignInModal = () => {
+        setShowSignInModal(false);
+    };
+
+    const handleShowSignUpModal = () => {
+        setShowSignUpModal(true);
+    };
+
+    const handleHideSignUpModal = () => {
+        setShowSignUpModal(false);
+    };
+
+    const renderHeaderForUnauthenticatedUser = () => {
+        return (
+            <>
+                <div
+                    onClick={handleShowSignInModal}
+                    className={'link'}
+                >
+                    My profile
+                </div>
+                <SignInModal
+                    show={showSignInModal}
+                    onHide={handleHideSignInModal}
+                    onShowSignUpModal={handleShowSignUpModal}
+                />
+                <SignUpModal
+                    show={showSignUpModal}
+                    onHide={handleHideSignUpModal}
+                    onShowSignInModal={handleShowSignInModal}
+                />
+            </>
+        )
+    }
 
     const renderHeaderForAuthenticatedUser = () => {
         return (
@@ -40,6 +84,7 @@ const Header = () => {
             >
                 Birthday Reminder
             </h1>
+            {!getToken() && renderHeaderForUnauthenticatedUser()}
             {getToken() && renderHeaderForAuthenticatedUser()}
         </header>
     );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,69 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Header from '../components/Header';
-import Button from '../components/Button';
-import SignInModal from '../components/SignInModal';
-import SignUpModal from '../components/SignUpModal';
 import Footer from '../components/Footer';
 
 const Main = () => {
-    const [showSignInModal, setShowSignInModal] = useState(false);
-    const [showSignUpModal, setShowSignUpModal] = useState(false);
-
-    const getToken = () => {
-        return localStorage.getItem('token');
-    };
-
-    const handleShowSignInModal = () => {
-        setShowSignInModal(true);
-    };
-
-    const handleHideSignInModal = () => {
-        setShowSignInModal(false);
-    };
-
-    const handleShowSignUpModal = () => {
-        setShowSignUpModal(true);
-    };
-
-    const handleHideSignUpModal = () => {
-        setShowSignUpModal(false);
-    };
-
-    const renderHeaderForUnauthenticatedUser = () => {
-        return (
-            <>
-                <Button
-                    text={'Sign In'}
-                    onClick={handleShowSignInModal}
-                />
-                <SignInModal
-                    show={showSignInModal}
-                    onHide={handleHideSignInModal}
-                />
-                <br/>
-                <Button
-                    text={'Sign Up'}
-                    onClick={handleShowSignUpModal}
-                />
-                <SignUpModal
-                    show={showSignUpModal}
-                    onHide={handleHideSignUpModal}
-                />
-            </>
-        )
-    }
-
     return (
-        <>
-            <center className={'content'}>
-                <Header/>
-                <main>
-                    <h1>hello friend</h1>
-                    {!getToken() && renderHeaderForUnauthenticatedUser()}
-                </main>
-                <Footer className={'footer'}/>
-            </center>
-        </>
+        <center className={'content'}>
+            <Header/>
+            <main>
+                <h1>hello friend</h1>
+            </main>
+            <Footer className={'footer'}/>
+        </center>
     );
 };
 
