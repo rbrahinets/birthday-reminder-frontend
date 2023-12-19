6ea16b31cced6e359de02fa7fc566f8f84243e68
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-19 20:04:28 +0200

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,100 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Footer = () => (
     <footer id={'footer'}>
-        <span className={'bold'}>Created by:</span>
-        <br/>
-        <span className={'bold'}>Developer:</span> Rostyslav Brahinets
-        (rostyslav.brahinets@gmail.com)
-        <br/>
-        <span className={'bold'}>Designer:</span> Yana Tkalych
-        (yanatkalych6@gmail.com)
+        <div>
+            <img
+                src={process.env.PUBLIC_URL + '/logo.png'}
+                alt={'logo'}
+                className={'footer-logo'}
+            />
+            <div>Copyright &copy; 2023 All Rights Reserved</div>
+        </div>
+        <div>
+            <h3>Contacts</h3>
+            <Link
+                to={'https://www.linkedin.com/in/rbrahinets'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/linkedin.png'}
+                    alt={'linkedin'}
+                    className={'footer-link-logo'}
+                />
+                <span>LinkedIn: Rostyslav Brahinets</span>
+            </Link>
+            <br/>
+            <Link
+                to={'https://www.linkedin.com/in/%D1%8F%D0%BD%D0%B0-%D1%82%D0%BA%D0%B0%D0%BB%D0%B8%D1%87-6a332528b/'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/linkedin.png'}
+                    alt={'linkedin'}
+                    className={'footer-link-logo'}
+                />
+                <span>LinkedIn: Yana Tkalych</span>
+            </Link>
+        </div>
+        <div>
+            <h3>Info</h3>
+            <Link
+                to={'https://github.com/rbrahinets/birthday-reminder-frontend'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/github.png'}
+                    alt={'github'}
+                    className={'footer-link-logo'}
+                />
+                <span>GitHub Front-end</span>
+            </Link>
+            <br/>
+            <Link
+                to={'https://github.com/rbrahinets/birthday-reminder-backend'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/github.png'}
+                    alt={'github'}
+                    className={'footer-link-logo'}
+                />
+                <span>GitHub Back-end</span>
+            </Link>
+        </div>
+        <div>
+            <h3>Follow us</h3>
+            <Link
+                to={'https://instagram.com/rbrahinets'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/instagram.png'}
+                    alt={'instagram'}
+                    className={'footer-link-logo'}
+                />
+                <span>@rbrahinets</span>
+            </Link>
+            <br/>
+            <Link
+                to={'https://instagram.com/yana_tkalych'}
+                target={'_blank'}
+                className={'footer-link icon-container'}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + '/instagram.png'}
+                    alt={'instagram'}
+                    className={'footer-link-logo'}
+                />
+                <span>@yana_tkalych</span>
+            </Link>
+        </div>
     </footer>
 );
 
