c6a64c86062f2cdeb899a37ca1b894df90b85942
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-19 15:09:23 +0200

    Add page for profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Main from './pages/Main';
+import Profile from './pages/Profile';
 import PageNotFound from './pages/PageNotFound';
 
 const url = '/birthday-reminder-frontend';
@@ -10,6 +11,7 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path={`${url}/`} element={<Main/>}/>
+                <Route path={`${url}/profile`} element={<Profile/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </Router>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {baseUrl} from '../constants';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
+
+const PageNotFound = () => {
+    const navigate = useNavigate();
+
+    const renderProfile = () => {
+        return (
+            <>
+                <h1>Profile</h1>
+                <Button
+                    text={'Sign Out'}
+                    onClick={handleSignOut}
+                />
+            </>
+        );
+    }
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token');
+        navigate(`${baseUrl}/`);
+    }
+
+    return (
+        <center>
+            <Header/>
+            <main>
+                {renderProfile()}
+            </main>
+            <Footer/>
+        </center>
+    );
+}
+
+export default PageNotFound;
