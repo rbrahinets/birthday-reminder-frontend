ae85f37498c669efb099cba19c813b7f9e162f99
rbrahinets <rostyslav.brahinets@gmail.com>
2023-12-27 17:36:07 +0200

    Add state with redux to Authentication component

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,89 +1,56 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {actionCreators} from '../state';
-import Button from './Button';
 import SignInModal from './SignInModal';
 import SignUpModal from './SignUpModal';
 import WaitModal from './WaitModal';
 
 const Authentication = () => {
     const dispatch = useDispatch();
-    const [showSignInModal, setShowSignInModal] = useState(false);
-    const [showSignUpModal, setShowSignUpModal] = useState(false);
-    const [showWaitModal, setShowWaitModal] = useState(false);
 
-    const {isAuthenticated} = useSelector((state) => state.isAuthenticated);
-    const {setIsAuthenticated} = bindActionCreators(
+    const {isVisibleSignInModal} = useSelector((state) => state.isVisibleSignInModal);
+    const {isVisibleSignUpModal} = useSelector((state) => state.isVisibleSignUpModal);
+    const {isVisibleWaitModal} = useSelector((state) => state.isVisibleWaitModal);
+
+    const {
+        setIsVisibleSignInModal,
+        setIsVisibleSignUpModal,
+        setIsVisibleWaitModal,
+        setIsAuthenticated,
+    } = bindActionCreators(
         actionCreators,
         dispatch
     );
 
-    const render = () => {
+    const renderAuthentication = () => {
         return (
             <>
-                <Button
-                    text={'Sign In'}
-                    onClick={handleShowSignInModal}
-                />
-                <br/>
-                <Button
-                    text={'Sign Up'}
-                    onClick={handleShowSignUpModal}
-                />
                 <SignInModal
-                    show={showSignInModal}
-                    onHide={handleHideSignInModal}
-                    onShowWaitModal={handleShowWaitModal}
-                    onHideWaitModal={handleHideWaitModal}
-                    onShowSignUpModal={handleShowSignUpModal}
-                    onSignInSuccess={handleSignInSuccess}
+                    show={isVisibleSignInModal}
+                    onHide={() => setIsVisibleSignInModal(false)}
+                    onShowWaitModal={() => setIsVisibleWaitModal(true)}
+                    onHideWaitModal={() => setIsVisibleWaitModal(false)}
+                    onShowSignUpModal={() => setIsVisibleSignUpModal(true)}
+                    onSignInSuccess={() => setIsAuthenticated(true)}
                 />
                 <SignUpModal
-                    show={showSignUpModal}
-                    onHide={handleHideSignUpModal}
-                    onShowWaitModal={handleShowWaitModal}
-                    onHideWaitModal={handleHideWaitModal}
-                    onShowSignInModal={handleShowSignInModal}
+                    show={isVisibleSignUpModal}
+                    onHide={() => setIsVisibleSignUpModal(false)}
+                    onShowWaitModal={() => setIsVisibleWaitModal(true)}
+                    onHideWaitModal={() => setIsVisibleWaitModal(false)}
+                    onShowSignInModal={() => setIsVisibleSignInModal(true)}
                 />
                 <WaitModal
-                    show={showWaitModal}
+                    show={isVisibleWaitModal}
                 />
             </>
         );
     }
 
-    const handleShowSignInModal = () => {
-        setShowSignInModal(true);
-    }
-
-    const handleHideSignInModal = () => {
-        setShowSignInModal(false);
-    }
-
-    const handleSignInSuccess = () => {
-        setIsAuthenticated(!isAuthenticated)
-    }
-
-    const handleShowSignUpModal = () => {
-        setShowSignUpModal(true);
-    }
-
-    const handleHideSignUpModal = () => {
-        setShowSignUpModal(false);
-    }
-
-    const handleShowWaitModal = () => {
-        setShowWaitModal(true);
-    }
-
-    const handleHideWaitModal = () => {
-        setShowWaitModal(false);
-    }
-
     return (
         <>
-            {render()}
+            {renderAuthentication()}
         </>
     )
 }
