133fc11d6a790730ecc4f9ae4f976191cbcfabed
rbrahinets <rostyslav.brahinets@gmail.com>
2024-01-05 14:58:51 +0200

    Add page for adding new friend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Main from './pages/Main';
 import Birthdays from './pages/Birthdays';
+import NewFriend from './pages/NewFriend';
 import Profile from './pages/Profile';
 import PageNotFound from './pages/PageNotFound';
 
@@ -13,6 +14,7 @@ const App = () => {
             <Routes>
                 <Route path={`${url}/`} element={<Main/>}/>
                 <Route path={`${url}/birthdays`} element={<Birthdays/>}/>
+                <Route path={`${url}/new-friend`} element={<NewFriend/>}/>
                 <Route path={`${url}/profile`} element={<Profile/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
---
+++ src/pages/NewFriend.jsx
@@ -0,0 +1,160 @@
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {bindActionCreators} from 'redux';
+import {actionCreators} from '../state';
+import Header from '../components/Header';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
+import friendService from '../services/FriendService';
+import {baseUrl} from '../constants';
+
+const NewFriend = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {errorMessages} = useSelector((state) => state.errorMessages);
+
+    const {setErrorMessages} = bindActionCreators(
+        actionCreators,
+        dispatch
+    );
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div className={'error'}>{errorMessages.message}</div>
+        );
+
+    const renderNewFriend = () => {
+        return (
+            <>
+                <h1>New Friend</h1>
+                <form className={'form'}>
+                    <Input
+                        type={'text'}
+                        name={'firstName'}
+                        id={'firstName'}
+                        placeholder={'First Name'}
+                        error={renderErrorMessage('firstName')}
+                    />
+                    <Input
+                        type={'text'}
+                        name={'lastName'}
+                        id={'lastName'}
+                        placeholder={'Last Name'}
+                        error={renderErrorMessage('lastName')}
+                    />
+                    <Input
+                        type={'text'}
+                        name={'email'}
+                        id={'email'}
+                        placeholder={'Email'}
+                        error={renderErrorMessage('email')}
+                    />
+                    <Input
+                        type={'date'}
+                        name={'dateOfBirth'}
+                        id={'dateOfBirth'}
+                        error={renderErrorMessage('dateOfBirth')}
+                    />
+                </form>
+                <Button
+                    text={'Add'}
+                    onClick={handleAdd}
+                />
+                <br/>
+                <Button
+                    text={'Cancel'}
+                    onClick={handleCancel}
+                />
+            </>
+        );
+    }
+
+    const errors = {
+        firstName: 'Invalid First Name',
+        lastName: 'Invalid Last Name',
+        email: 'Invalid Email',
+        dateOfBirth: 'Invalid Date Of Birth',
+    }
+
+    const handleAdd = async (event) => {
+        event.preventDefault();
+
+        let {firstName, lastName, email, dateOfBirth} =
+            document.forms[0];
+        let isValidInputtedData = false;
+
+        if (
+            !firstName.value ||
+            !firstName.value.trim().length
+        ) {
+            setErrorMessages({
+                name: 'firstName',
+                message: errors.firstName,
+            });
+        } else if (
+            !lastName.value ||
+            !lastName.value.trim().length
+        ) {
+            setErrorMessages({
+                name: 'lastName',
+                message: errors.lastName,
+            });
+        } else if (
+            !email.value ||
+            !email.value.trim().length ||
+            !isValidEmail(email.value)
+        ) {
+            setErrorMessages({
+                name: 'email',
+                message: errors.email,
+            });
+        } else if (
+            !dateOfBirth.value
+        ) {
+            setErrorMessages({
+                name: 'dateOfBirth',
+                message: errors.dateOfBirth,
+            });
+        } else {
+            isValidInputtedData = true;
+        }
+
+        if (isValidInputtedData) {
+            try {
+                await friendService.save({
+                    firstName: firstName.value,
+                    lastName: lastName.value,
+                    email: email.value,
+                    dateOfBirth: dateOfBirth.value,
+                });
+                navigate(`${baseUrl}/birthdays`);
+            } catch (error) {
+                console.error('Adding New Friend Failed', error);
+                alert('Adding New Friend Failed. The e-mail you entered already exist!');
+            }
+        }
+    }
+
+    const isValidEmail = (email) => {
+        return email.includes('@') && email.endsWith('.com');
+    }
+
+    const handleCancel = async () => {
+        navigate(`${baseUrl}/birthdays`);
+    }
+
+    return (
+        <center className={'container'}>
+            <Header/>
+            <main>
+                {renderNewFriend()}
+            </main>
+            <Footer/>
+        </center>
+    );
+}
+
+export default NewFriend;
