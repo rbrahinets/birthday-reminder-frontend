a21911d92e0aca165405dd748f2c2dfbbcf83973
rbrahinets <rostyslav.brahinets@gmail.com>
2024-02-07 22:13:37 +0200

    Update Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Input = ({type, name, id, placeholder, error}) => (
+const Input = ({type, name, id, placeholder, error, defaultValue}) => (
     <div className={'formcontrol'}>
         <input
             type={type}
             name={name}
             id={id}
             placeholder={placeholder}
+            defaultValue={defaultValue}
             required
         />
         {error}
