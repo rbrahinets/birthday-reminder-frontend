641e644021b2388db79c2e09cbe564818ee2c6dc
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-04 15:54:57 +0300

    Update redirecting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Main from './pages/Main';
 import Birthdays from './pages/Birthdays';
 import BirthdayInfo from './pages/BirthdayInfo';
