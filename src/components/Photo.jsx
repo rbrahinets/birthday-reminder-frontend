62d7c6f7bb5202a75eae1a175f04ad5c89c095d4
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-08 17:49:57 +0300

    Add component for photo

---
+++ src/components/Photo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Photo.css';
+
+const Photo = ({src, alt, onClick}) => (
+    <img
+        src={src}
+        alt={alt}
+        className={'photo'}
+        onClick={onClick}
+    />
+);
+
+export default Photo;
