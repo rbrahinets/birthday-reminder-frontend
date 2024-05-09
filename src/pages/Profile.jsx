5c60c4bd60cf3bc25e95a599ee86a5554756d614
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-09 21:37:18 +0300

    Add Firebase image to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,31 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {deleteObject, getDownloadURL, ref, uploadBytes} from 'firebase/storage';
-import {v4} from 'uuid';
 import {actionCreators} from '../state';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import WaitModal from '../components/WaitModal';
-import Photo from '../components/Photo';
+import FirebaseImage from '../components/FirebaseImage';
 import Footer from '../components/Footer';
 import userService from '../services/UserService';
-import {firebaseStorage} from '../configs/firebase';
 import './Profile.css';
 
 const Profile = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const fileInputRef = useRef(null);
 
     const {currentUser} = useSelector((state) => state.currentUser);
     const {loading} = useSelector((state) => state.loading);
     const {profileImage} = useSelector((state) => state.profileImage);
-    const {previewImage} = useSelector((state) => state.previewImage);
-
-    const [imageFile, setImageFile] = useState(null);
+    const {previewProfileImage} = useSelector((state) => state.previewProfileImage);
 
     const {
         setIsAuthenticated,
         setCurrentUser,
         setLoading,
         setProfileImage,
-        setPreviewImage,
+        setPreviewProfileImage,
     } = bindActionCreators(
         actionCreators,
         dispatch
@@ -60,31 +54,18 @@ const Profile = () => {
     const getProfileInfo = () => {
         return (
             <div className={'profile-info'}>
-                <Photo
-                    src={previewImage || profileImage}
-                    alt={'profile-image'}
-                    onClick={handleImageClick}
+                <FirebaseImage
+                    defaultImageUrl={`${process.env.PUBLIC_URL}/add.png`}
+                    object={currentUser}
+                    state={{
+                        firebaseImage: profileImage,
+                        previewFirebaseImage: previewProfileImage,
+                        setFirebaseImage: setProfileImage,
+                        setPreviewFirebaseImage: setPreviewProfileImage,
+                    }}
+                    service={userService}
+                    resetObject={fetchCurrentUserData}
                 />
-                <input
-                    type={'file'}
-                    ref={fileInputRef}
-                    accept={'image/*'}
-                    style={{display: 'none'}}
-                    onChange={handleFileChange}
-                />
-                <br/>
-                {profileImage && !previewImage && !profileImage.includes('/add.png') && (
-                    <Button
-                        text={'Delete Image'}
-                        onClick={handleDeleteImage}
-                    />
-                )}
-                {previewImage && (
-                    <Button
-                        text={'Save'}
-                        onClick={handleSave}
-                    />
-                )}
                 <div>{currentUser.firstName} {currentUser.lastName}</div>
                 <div>{currentUser.email}</div>
             </div>
@@ -108,88 +89,6 @@ const Profile = () => {
                 />
             </>
         );
-    }
-
-    const handleDeleteImage = async () => {
-        setLoading(true);
-
-        setProfileImage(process.env.PUBLIC_URL + '/add.png');
-
-        let oldImage = null;
-
-        if (
-            currentUser.imageUrl &&
-            currentUser.imageUrl.trim().length > 0
-        ) {
-            oldImage = ref(firebaseStorage, currentUser.imageUrl);
-        }
-
-        const updatedUser = {
-            ...currentUser,
-            imageUrl: "",
-        }
-
-        await userService.update(updatedUser._id, updatedUser);
-
-        if (oldImage) {
-            deleteObject(oldImage).then();
-        }
-
-        setLoading(false);
-    }
-
-    const handleImageClick = () => {
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
-        }
-    }
-
-    const handleFileChange = async (e) => {
-        e.preventDefault();
-
-        const file = e.target.files[0];
-
-        if (file) {
-            setPreviewImage(URL.createObjectURL(file));
-            setImageFile(file);
-        }
-    }
-
-    const handleSave = async () => {
-        setLoading(true);
-
-        if (imageFile) {
-            setPreviewImage(null);
-        }
-
-        let oldImage = null;
-
-        if (
-            currentUser.imageUrl &&
-            currentUser.imageUrl.trim().length > 0
-        ) {
-            oldImage = ref(firebaseStorage, currentUser.imageUrl);
-        }
-
-        const imageRef = ref(firebaseStorage, `images/${v4()}-${imageFile.name}`);
-        const snapshot = await uploadBytes(imageRef, imageFile);
-        const url = await getDownloadURL(snapshot.ref);
-
-        setProfileImage(url);
-        setImageFile(null);
-
-        const updatedUser = {
-            ...currentUser,
-            imageUrl: url,
-        }
-
-        await userService.update(updatedUser._id, updatedUser);
-
-        if (oldImage) {
-            deleteObject(oldImage).then();
-        }
-
-        setLoading(false);
     }
 
     const handleEdit = () => {
