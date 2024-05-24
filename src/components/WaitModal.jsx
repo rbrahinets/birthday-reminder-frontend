006269948f57700f780bb7503189e6f280b2ef14
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-24 12:37:37 +0300

    Update styles for loading

--- src/components/WaitModal.jsx
+++ src/components/WaitModal.jsx
@@ -7,6 +7,7 @@ const WaitModal = ({show}) => {
         <Modal
             show={show}
             dialogClassName={'modal-wait'}
+            className={'center'}
         >
             <img
                 className={'loading'}
