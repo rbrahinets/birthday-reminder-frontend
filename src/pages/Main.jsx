e824460c1e7ef23c074192b26fde8afe35e2d2f5
rbrahinets <rostyslav.brahinets@gmail.com>
2024-09-21 22:03:55 +0300

    Add saving new user to database

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useAuth0} from '@auth0/auth0-react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import userService from '../services/UserService';
 import './Main.css';
 
 const Main = () => {
+  const {user, isAuthenticated, isLoading} = useAuth0();
+
   const renderPage = () => {
     return (
       <>
@@ -22,6 +26,41 @@ const Main = () => {
       </>
     );
   }
+
+  const getUserByEmail = async () => {
+    if (isAuthenticated) {
+      try {
+        localStorage.setItem('isSavedUser', 'true');
+        localStorage.setItem('currentUserEmail', user.email);
+        return await userService.findByEmail(user.email);
+      } catch (error) {
+        return undefined;
+      }
+    }
+  }
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!user || !isAuthenticated || localStorage.getItem('isSavedUser') === 'true') return;
+
+      const savedUser = await getUserByEmail(user.email);
+      if (!!savedUser) return;
+
+      const [firstName, lastName] = user.nickname.split('.');
+
+      try {
+        await userService.save({
+          firstName: firstName,
+          lastName: lastName,
+          email: user.email,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUserData().then();
+  }, [user, isAuthenticated]);
 
   return (
     <div className={'container center'}>
