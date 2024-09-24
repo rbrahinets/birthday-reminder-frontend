753d5ce21ae52136fa753cc9f8410bfc8e466606
rbrahinets <rostyslav.brahinets@gmail.com>
2024-09-24 22:59:30 +0300

    Update profile info

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {actionCreators} from '../state';
 
-const ProfileInfo = () => {
+const ProfileInfo = ({updateProfileInfo}) => {
+  const dispatch = useDispatch();
+
   const {currentUser} = useSelector((state) => state.currentUser);
+
+  const {
+    setLoading,
+  } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  useEffect(() => {
+    setLoading(true);
+    updateProfileInfo().then(() => setLoading(false));
+  }, []);
 
   return (
     <div className={'info-container'}>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -72,7 +72,7 @@ const Profile = () => {
         />
         {
           isProfileInfoMode ?
-            <ProfileInfo data={fetchCurrentUserData}/> :
+            <ProfileInfo updateProfileInfo={fetchCurrentUserData}/> :
             <ProfileEdit/>
         }
         {isProfileInfoMode &&
