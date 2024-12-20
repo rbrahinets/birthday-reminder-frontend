a8f54f66f06dc043ca083de4f470f7d1951761c2
Rostyslav Brahinets <rostyslav.brahinets@gmail.com>
2024-12-20 19:10:18 +0200

    Update adaptive visibility for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,44 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {useSelector} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {useTranslation} from 'react-i18next'
-import {CgProfile} from 'react-icons/cg'
-import {FaBars} from 'react-icons/fa'
-import LinkButton from './LinkButton'
-import './Header.css'
-import './LanguageSelector.css'
-
+import React, {useEffect, useRef, useState} from 'react';
+import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
+import {CgProfile} from 'react-icons/cg';
+import {FaBars} from 'react-icons/fa';
+import LinkButton from './LinkButton';
+import './Header.css';
 
 const Header = () => {
-  const {t} = useTranslation()
-  const {isDarkMode} = useSelector((state) => state.isDarkMode)
+  const {t} = useTranslation();
+  const {isDarkMode} = useSelector((state) => state.isDarkMode);
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const menuRef = useRef(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleRef = useRef(null);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const closeMenu = () => {
-    setIsMenuOpen(false)
-  }
+    setIsMenuOpen(false);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        closeMenu()
+      if (
+        menuRef.current && !menuRef.current.contains(event.target) &&
+        toggleRef.current && !toggleRef.current.contains(event.target)
+      ) {
+        closeMenu();
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener('mousedown', handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [menuRef])
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef, toggleRef]);
 
   const renderLinks = () => (
     <div className={`link-container ${isMenuOpen ? 'open' : ''}`} ref={menuRef}>
@@ -59,33 +61,33 @@ const Header = () => {
         {t('contacts')}
       </a>
     </div>
-  )
+  );
 
   const renderHeader = () => {
     return (
       <div className={'header-content'}>
         <Link
           to={`/`}
-          className="logo"
+          className={'logo'}
         >
           <img
             src={process.env.PUBLIC_URL + '/logo.png'}
             alt={'logo'}
           />
         </Link>
-        <div className="menu-toggle" onClick={toggleMenu}>
+        <div className={'menu-toggle'} onClick={toggleMenu} ref={toggleRef}>
           <FaBars/>
         </div>
         {renderLinks()}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <header className={`${isDarkMode ? 'dark' : 'light'}-header`}>
       {renderHeader()}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
