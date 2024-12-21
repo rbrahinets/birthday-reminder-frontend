9e0b4877815895a36ffb8870f422ef8ce440c94c
Rostyslav Brahinets <rostyslav.brahinets@gmail.com>
2024-12-21 12:44:30 +0200

    Fix zooming on mobile

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import {store} from './state';
 import App from './App';
 import './utils/i18n';
 
-window.onload = () => {
-  document.body.style.transform = 'scale(0.8)';
-  document.body.style.transformOrigin = 'top left';
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
